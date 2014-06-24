e3156a53d02ed2a430004b8f5fae5754adcdb41b
Dylan Katz <dylanishappy1@gmail.com>
2014-06-24 12:02:11 -0600

    Tweaked readiness delay

--- teleprescenceclient.js
+++ teleprescenceclient.js
@@ -1,25 +1,30 @@
-var bodyElem = $("body");
-var keysPressed = [];
-while(!gapi.hangout.isApiReady()) {};
-gapi.hangout.data.setValue("keys", JSON.stringify(keysPressed));
-bodyElem.keydown(function(evt) {
-  keysPressed.push(evt.which);
-  if(keys.indexOf(evt.which) == -1)
-    gapi.hangout.data.setValue("keys", JSON.stringify(keysPressed));
-});
-bodyElem.keyup(function(evt) {
-  var index = keysPressed.indexOf(evt.which);
-  if(index != -1) {
-    keysPressed.splice(index, 1);
-    gapi.hangout.data.setValue("keys", JSON.stringify(keysPressed));
-  }
-});
-var lastData = gapi.hangout.data.getValue("keys");
-while(true) {
-  console.log(gapi.hangout.getKeys());
-  if(gapi.hangout.data.getValue("keys") != lastData) {
-    alert(gapi.hangout.data.getValue("keys"));
-    lastData = gapi.hangout.data.getValue("keys");
-    alert(lastData);
+function init() {
+  var bodyElem = $("body");
+  var keysPressed = [];
+  while(!gapi.hangout.isApiReady()) {};
+  gapi.hangout.data.setValue("keys", JSON.stringify(keysPressed));
+  bodyElem.keydown(function(evt) {
+    keysPressed.push(evt.which);
+    if(keys.indexOf(evt.which) == -1)
+      gapi.hangout.data.setValue("keys", JSON.stringify(keysPressed));
+  });
+  bodyElem.keyup(function(evt) {
+    var index = keysPressed.indexOf(evt.which);
+    if(index != -1) {
+      keysPressed.splice(index, 1);
+      gapi.hangout.data.setValue("keys", JSON.stringify(keysPressed));
+    }
+  });
+  var lastData = gapi.hangout.data.getValue("keys");
+  while(true) {
+    console.log(gapi.hangout.getKeys());
+    if(gapi.hangout.data.getValue("keys") != lastData) {
+      alert(gapi.hangout.data.getValue("keys"));
+      lastData = gapi.hangout.data.getValue("keys");
+      alert(lastData);
+    }
   }
 }
+gapi.hangout.onApiReady.add(function(eventObj) {
+  init();
+});
