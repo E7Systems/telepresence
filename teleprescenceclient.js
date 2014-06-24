053e48f50c584fcca45516183b06f970da8f7395
Dylan Katz <dylanishappy1@gmail.com>
2014-06-24 11:49:36 -0600

    Stringified Array

--- teleprescenceclient.js
+++ teleprescenceclient.js
@@ -4,13 +4,13 @@ gapi.hangout.data.setValue("keys", keysPressed);
 bodyElem.keydown(function(evt) {
   keysPressed.push(evt.which);
   if(keys.indexOf(evt.which) == -1)
-    gapi.hangout.data.setValue("keys", keysPressed);
+    gapi.hangout.data.setValue("keys", JSON.stringify(keysPressed));
 });
 bodyElem.keyup(function(evt) {
   var index = keysPressed.indexOf(evt.which);
   if(index != -1) {
     keysPressed.splice(index, 1);
-    gapi.hangout.data.setValue("keys", keysPressed);
+    gapi.hangout.data.setValue("keys", JSON.stringify(keysPressed));
   }
 });
 var lastData = gapi.hangout.data.getValue("keys");
