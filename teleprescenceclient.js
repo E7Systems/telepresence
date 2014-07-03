fa532ca5ff771da3774ff6e06c43e9bf99246dc2
Dylan Katz <dylanishappy1@gmail.com>
2014-07-03 13:31:54 -0600

    Updated to implement more data functions

--- teleprescenceclient.js
+++ teleprescenceclient.js
@@ -21,9 +21,46 @@ function init() {
     if(gapi.hangout.data.getValue("keys") != lastData) {
       lastData = gapi.hangout.data.getValue("keys");
       console.log("New Keys: " + lastData);
+      //$.get("localhost:8080")
     }
   }, 50, 50);
 }
 gapi.hangout.onApiReady.add(function(eventObj) {
   init();
 });
+
+function up() {
+  keysPressed.push(38);
+}
+function down() {
+  keysPressed.push(40);
+}
+function left() {
+  keysPressed.push(37);
+}
+function right() {
+  keysPressed.push(39);
+}
+function setData(key, value) {
+	gapi.hangout.setValue(key, value);
+}
+function generateSecureKey() {
+	gapi.hangout.layout.displayNotice("Generating a secure key for your channel, please move your mouse at random for a few seconds.", false);
+	int[] seeds = new Array();
+	int lastX = 0;
+	int lastY = 0;
+	$("body").append("<div id='randGen'></div>");
+	$("#randGen").mousemove(function(evt) {
+		if(Math.round(Math.random()*100) <= 30)
+			seeds.push(Math.abs(lastX - evt.X) * Math.abs(lastY - evt.Y));
+		if(seeds.length >= 5) {
+			$("#randGen").unbind();
+			gapi.hangout.layout.dismissNotice();
+			gapi.hangout.layout.displayNotice("Generation finished.", true);	
+			console.log(seeds.join(","));
+		}
+	});
+}
+function appendData(key, value) {
+	gapi.hangout.setValue(key, gapi.hangout.getValue(key)+value);
+}
